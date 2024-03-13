40fb944263e6636b34788cdf00f8b975cc2fdbd1
Joel Uzcategui <Joeluzcategui102@gmail.com>
2024-03-13 14:21:12 -0400

    Increase FAB size

--- src/lib/components/fab.svelte
+++ src/lib/components/fab.svelte
@@ -16,7 +16,7 @@
     type="button"
     on:click={contactMe}
     class="mb-2 inline-block rounded-full bg-[#128c7e] p-3 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">
-    <span class="[&>svg]:h-4 [&>svg]:w-4">
+    <span class="[&>svg]:h-8 [&>svg]:w-8">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
